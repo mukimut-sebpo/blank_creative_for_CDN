b528cbe47a6fa4372236583717fd0ef9eb36a69e
mukimut-sebpo <mukimut.salat@sebpo.com>
2023-10-30 13:49:12 +0600

    first commit

---
+++ do_not_delete/manifest.js
@@ -0,0 +1,19 @@
+FT.manifest({
+    "filename": "index.html",
+    "width": 300,
+    "height": 250,
+    "clickTagCount": 0,
+    "hideBrowsers": ["ie8"],
+    "richLoads": [
+        { "name": "Main_RL","src": "mailchimp_fonts_rl" }
+    ],
+	"instantAds": [
+			{"name": "Main_RL", "type": "richload", "default": "mailchimp_fonts_rl"},
+			{"name":"image1",   "type":"image", "default":"images/blank.png"},
+			{"name":"image2",   "type":"image", "default":"images/blank.png"},
+			{"name":"image3",   "type":"image", "default":"images/blank.png"},
+			{"name":"image4",   "type":"image", "default":"images/blank.png"},
+			{"name":"image5",   "type":"image", "default":"images/blank.png"},
+			{"name":"image6",   "type":"image", "default":"images/blank.png"}
+		]
+});
